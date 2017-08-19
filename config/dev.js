f6f18b506823f6ac791ee80fc0fe1cf504f6f247
Philip Balbas <philip.balbas@gmail.com>
2017-08-19 08:51:07 +0800

    configure prod and dev keys

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '1054204510258-ql9im7v9shpk0c24r34qq6krqpj5n3fr.apps.googleusercontent.com',
+  googleClientSecret: '1N5OEC9KI3MPS9N74VNjy3Vs',
+  mongoURI: 'mongodb://yves:yves@ds043952.mlab.com:43952/emaily-dev',
+  cookieKey: 'aboijkngtnrlafuinwoighnrwkguwh4otln4qlitui4ngrknhw5hrgroig',
+}
